0499a9e00d6fd2c56f9d4aeeacc9a289235c28d8
Vishal.AI <132605163+vishalP-08@users.noreply.github.com>
2023-08-16 16:54:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function decimalToBinary(num) {
-  //Write you code here
+  if (num === 0) {
+        return '0';
+    }
+
+    let binary = '';
+    while (num > 0) {
+        binary = (num % 2) + binary;
+        num = Math.floor(num / 2);
+    }
+
+    return binary;
   
 }
 
